b4f93f1c0e469af6dc13f27e6ac07120ab2db3c3
samdevmeshach <samdevmeshach@users.noreply.github.com>
2019-05-30 22:50:23 +0530

    Create geeksCoffee.java
    greeks coffee Problem

---
+++ geeksCoffee.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class geeksCoffee
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner (System.in); 
+		int n,m,i;
+		System.out.println("Enter Number : ");
+		n = in.nextInt();
+        System.out.println("Enter Number : ");
+        m = in.nextInt();
+		for (i = 1; i < m; i++)       // Loop for calculating Mth unit coffee Consumed
+        {
+            n=n/2;
+        }
+        System.out.println(n);		// Total Number of units
+	}
+}
