be2e938892fe8d74b77214c93d7c1094db51accc
samdevmeshach <samdevmeshach@users.noreply.github.com>
2019-06-04 15:30:00 +0530

    Update MoveWhiteSpaceToLeft.java
    Changes Using Method

--- MoveWhiteSpaceToLeft.java
+++ MoveWhiteSpaceToLeft.java
@@ -4,40 +4,20 @@ public class MoveWhiteSpaceToLeft
     public static void main(String[] args)
     {
         Scanner in = new Scanner (System.in); 
-        int i,j=0,z=0,count = 0,n,t;
-        char temp;
-        String c1 = new String();
+        int count = 0;
+        String output;
         System.out.println("Enter String 1 : ");
-        c1 = in.nextLine();
-        char s1[]= c1.toCharArray();            // Converting String to Char Array
-        char s2[] = new char[20];
-        for (i = 0; i < s1.length; i++)           // Loop to find the Space count and make character alone in seperate Array
+        String s1 = in.nextLine();
+        
+        for (int i = 0; i < s1.length(); i++)           // Loop to find the Space count and make character alone in seperate Array
         {
-            if(s1[i] == ' ')
-            {
-                count = count + 1;
-                continue;
-            }
-            else
-            {
-                s2[z] = s1[i];          // Copying Character alone in tha Array
-                z++;
-            }
-            s2[z]='\0';                 // Assign Last Character As Null Character
+            if(s1.charAt(i)==" ")
+                    count++;
         }
-        for (i = 0; i < s1.length; i++)             // Loop to Make the Space in Left and Character On the Right
-        {
-            if(count != 0)                          // Assign Space Untill Count Value Get Zero
-            {
-                s1[i]=' ';
-                count--;
-            }
-            else
-            {
-                s1[i]=s2[j];                    // After Count Getting 0 Assign Character One By One In The Same Array
-                j++;
-            }
-        }
-        System.out.println(s1);        //Output String
-    }
+        for(int i=0; i<count; i++){
+                output+=" ";
+
+        } 
+        output +=  s1.replaceAll(" ","");        //Output String
+}
 }
